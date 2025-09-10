31eca7679b46f49e932cca226a074c7da442a7c8
Park Sehyuk <153153695+UndergroundAttic@users.noreply.github.com>
2025-09-10 14:56:03 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,7 @@
+m = {
+    "N": "North London Collegiate School",
+    "B": "Branksome Hall Asia",
+    "K": "Korea International School",
+    "S": "St. Johnsbury Academy",
+}
+print(m[input()[0]])
